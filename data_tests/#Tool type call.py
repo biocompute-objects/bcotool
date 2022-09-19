5c82514307ddf76a66d74c27d2602d357ed9b85c
Alhanouf-Altuwayjiri <112920623+Alhanouf-Altuwayjiri@users.noreply.github.com>
2022-09-19 11:33:49 -0400

    Update #Tool type call.py

--- data_tests/#Tool type call.py
+++ data_tests/#Tool type call.py
@@ -1,14 +1,29 @@
-#Tool type call
+# Alhanouf Altuwayjiri
 #the needed package to use JSON
 import requests
 # ask the user for tool name 
-inp=input("Tool Name?")
+def ToolType(inp):
 #use the templet http and add the tool name to it 
-x="https://bio.tools/api/t/?biotoolsID=%22%22&format=json"
-y=x[:39]+inp+x[39:]
+    x="https://bio.tools/api/t/?format=json&q=&sort=score"
+   
+    LINK=x[:39]+inp+x[39:]
+
 #reterive all the information about The input tool name and store them in the result
-result=requests.get((y))
+    result=requests.get((LINK))
 # change it from JSON to Python Dictionary 
-dic=result.json()
+    dic=result.json()
 #print the tool type for the used tool name
-print(dic['list'][0]['toolType'])
+
+    typet=[]
+    for i,key in enumerate(dic['list']):
+        x=(dic['list'][i]['function'][0]['operation'])
+        for r,key in enumerate(x):
+            y=(dic['list'][i]['function'][0]['operation'][r]['term'])
+            typet.append(y)
+    
+    h=(set(typet))
+    final=typet
+    for i in h:
+        final.remove(i)
+   
+    return set(final)
